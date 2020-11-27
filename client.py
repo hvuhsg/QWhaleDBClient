9573fb462bc83877a558f36572efe2b4b68cc190
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2020-11-27 15:14:29 +0200

    Base client created

---
+++ client.py
@@ -0,0 +1,62 @@
+import requests
+from pymongo import MongoClient
+from pymongo.database import Database
+
+
+__all__ = ["APIClient"]
+
+API_URL = "http://127.0.0.1:8000/api"
+
+
+class APIClient:
+    def __init__(self, token: str):
+        self._token = token
+        self._db_name = self._token.split(":")[0]
+        self._session: requests.sessions.Session = requests.session()
+        self._database_info = None
+        self._mongo_client: MongoClient = None
+        self.activated = False
+
+    @property
+    def database_info(self):
+        if self._database_info is None:
+            raise ValueError("Database isn't activated.")
+        return self._database_info
+
+    def activate_database(self):
+        result = self._session.get(API_URL+f"/activate/{self._token}")
+        assert result.status_code == 200, result.json()
+
+        activation_info = result.json()
+        self._database_info = activation_info
+        self.activated = True
+
+    def deactivate_database(self):
+        result = self._session.get(API_URL+f"/deactivate/{self._token}")
+        assert result.status_code == 200, result.json()
+
+        deactivation_info = result.json()
+        return deactivation_info
+
+    def get_database(self) -> Database:
+        if self._database_info is None:
+            self.activate_database()
+        client = MongoClient(self.database_info["mongo_url"])
+        self._mongo_client = client
+        db = client.get_database(self._db_name)
+        return db
+
+    def close(self):
+        try:
+            self._mongo_client.close()
+        finally:
+            self.activated = False
+            return self.deactivate_database()
+
+    def __enter__(self) -> Database:
+        self.activate_database()
+        db = self.get_database()
+        return db
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
