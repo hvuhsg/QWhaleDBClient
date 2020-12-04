c3f299931020eb1a5219dec53bed0b2364286bca
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2020-12-04 09:37:51 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_namespace_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="qwhale_client",  # How you named your package folder
+    packages=find_namespace_packages(),  # Chose the same as "name"
+    include_package_data=True,
+    version="v0.1.0",  # Start with a small number and increase it with every change you make
+    license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    description="Python client for Qwhale API",  # Give a short description about your library
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Yehoyada.s",  # Type in your name
+    author_email="hvuhsg6@gmail.com",  # Type in your E-Mail
+    url="https://qwhale.ml",  # Provide either the link to your github or to your website
+    download_url="https://github.com/hvuhsg/qwhale_client/archive/v0.1.0.tar.gz",
+    keywords=[
+        "API",
+        "Client",
+        "Qwhale",
+        "QWhale",
+        "client",
+        "storage",
+        "MongoDB"
+    ],  # Keywords that define your package best
+    install_requires=["pymongo", "requests"],  # I get to this in a second
+    classifiers=[
+        "Development Status :: 1 - Alpha",
+        "Intended Audience :: Developers",  # Define that your audience are developers
+        "Topic :: Software Development :: API Tools",
+        "License :: OSI Approved :: MIT License",  # Again, pick a license
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
